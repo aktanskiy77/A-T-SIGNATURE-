99cdf9c87b12f91deea8bf2233d327f289352611
aktanskiy77 <aktan0006@gmail.com>
2026-03-25 00:05:31 +0600

    Еще фикс 2

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -10,7 +10,6 @@ const ProductCard = ({ image, name, price, isFirst = false }) => {
       ${isFirst ? 'md:ml-[15%]' : ''}
     `}>
       <img src={image} alt={name} className="w-full aspect-[3/4] object-cover" />
-      
       <div className="pl-[4px] md:pl-[10px]">
         <p className='text-[8px] md:text-[16px] pt-[5px] md:pt-[10px] uppercase truncate leading-tight'>{name}</p>
         <p className='text-[9px] md:text-[16px] font-bold md:font-normal'>{price} SOM</p>
